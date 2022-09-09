e8c425e273510e7c495aa3c8c6646e0dacb30729
lihuanchen708 <837619128@qq.com>
2022-09-09 22:21:09 +0800

    更新接口

---
+++ app/Http/Controllers/EmpireUserController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\EmpireUser;
+use Illuminate\Http\Request;
+
+class EmpireUserController extends Controller
+{
+    /*
+     *
+     * */
+    public function login(string $vldKey = ""): \Illuminate\Http\JsonResponse
+    {
+        $data = [
+            'data' => [
+                'vldKey' => '',
+                'deadline' => ''
+            ],
+            'msg' => '使用码错误，请加v：clh_zgb',
+            'code' => '0',
+        ];
+        if (!empty($vldKey)){
+           $user =  EmpireUser::query()->first(["vld_key"=>$vldKey]);
+           if ($user){
+               if (time() >= $user->vld_key){
+                   $data["msg"] = "使用码已过期，请加v：clh_zgb ";
+               }else{
+                   $data["data"]["vldKey"] = $user->vld_key;
+                   $data["data"]["deadline"] = $user->deadline;
+                   $data["msg"] = "使用码到期时间为：" . date('Y-m-d H-i-s',$user->deadline);
+                   $data["code"] = 1;
+               }
+           }
+        }
+        return response()->json($data);
+    }
+}
--- database/migrations/2022_09_09_105533_create_empire_user_table.php
+++ database/migrations/2022_09_09_105533_create_empire_user_table.php
@@ -16,7 +16,7 @@ class CreateEmpireUserTable extends Migration
         Schema::create('empire_user', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('vld_key')->unique()->default("abc")->comment('使用码');
-            $table->time('deadline')->default(1662723111)->comment('到期时间戳');
+            $table->integer('deadline')->default(1662723111)->comment('到期时间戳');
             $table->timestamps();
         });
     }
